311f085934cb3da0ceca07045f48d476e66e5773
Nick Ng <ngyewch@gmail.com>
2025-04-29 20:34:19 +0800

    feat: porting (WIP)

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,81 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    import Swal from 'sweetalert2';
+    import ky from 'ky';
+    import {onMount} from 'svelte';
+
+    const baseUrl = 'https://script.google.com/macros/s/AKfycbym-ODmhPDbYIfd0oSaD2FYdtZCbpNyPWctBOQmu3C-_LbJQADixgXg-So8foXMXdSR/exec';
+    const unitPrice = 0.60;
+
+    interface User {
+        username: string;
+    }
+
+    let users = $state<User[]>([]);
+    let selectedUser = $state<string>();
+    let coffeeCount = $state<number>();
+
+    onMount(() => {
+        Swal.fire({
+            title: 'Retrieving user list...',
+            didOpen: () => {
+                Swal.showLoading();
+                ky.get(makeUrl({action: 'getUsers'}))
+                    .then(rsp => rsp.json())
+                    .then(rsp => {
+                        const userList = rsp as User[];
+                        userList
+                            .sort((a, b) => a.username.localeCompare(b.username, undefined, {sensitivity: 'accent'}));
+                        users = userList;
+                        Swal.close();
+                    });
+            },
+        });
+    });
+
+    function makeUrl(params: Record<string, string>): string {
+        const urlSearchParams = new URLSearchParams(params);
+        return `${baseUrl}?${urlSearchParams.toString()}`;
+    }
+
+    function onUserChanged(e: InputEvent) {
+        if (selectedUser === undefined) {
+            coffeeCount = undefined;
+            return;
+        }
+        Swal.fire({
+            title: 'Retrieving user info...',
+            didOpen: () => {
+                Swal.showLoading();
+                ky.get(makeUrl({action: 'getTotalCoffee', username: selectedUser ? selectedUser : ''}))
+                    .then(rsp => rsp.json())
+                    .then(rsp => {
+                        coffeeCount = rsp as number;
+                        Swal.close();
+                    });
+            },
+        });
+    }
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+    <span style="font-size: 10em;">&#9749</span>
+    <h1>ARL Productivity Logger</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+    {#if users.length > 0}
+        <select bind:value={selectedUser} onchange={onUserChanged}>
+            <option selected disabled value="">
+                Select user...
+            </option>
+            {#each users as user}
+                <option>{user.username}</option>
+            {/each}
+        </select>
+    {/if}
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    {#if (selectedUser !== undefined) && (coffeeCount !== undefined)}
+        &#9749 x {coffeeCount} @ ${unitPrice.toFixed(2)} = ${(coffeeCount * unitPrice).toFixed(2)}
+    {/if}
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
