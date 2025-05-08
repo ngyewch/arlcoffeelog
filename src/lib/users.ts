9bf070ce92f2ea96f4c1f004e6475d1043ec1ae1
Nick Ng <ngyewch@gmail.com>
2025-05-08 15:52:08 +0800

    feat: user mapping

---
+++ src/lib/users.ts
@@ -0,0 +1,51 @@
+export interface UserMapping {
+    username: string;
+    githubLogin: string;
+}
+
+export const existingUsers: UserMapping[] = [
+    {
+        username: "bharath", // Bharath
+        githubLogin: "bharathkalyan",
+    },
+    {
+        username: "hari",
+        githubLogin: "harivnkochi",
+    },
+    {
+        username: "kee", // Kee Boon Leng
+        githubLogin: "AlexK2008",
+    },
+    {
+        username: "kexin",
+        githubLogin: "Kexin1",
+    },
+    {
+        username: "luyuan",
+        githubLogin: "LuyuanPeng",
+    },
+    {
+        username: "mandar",
+        githubLogin: "mchitre",
+    },
+    {
+        username: "nick",
+        githubLogin: "ngyewch",
+    },
+    {
+        username: "rajat",
+        githubLogin: "rajmis",
+    },
+    {
+        username: "Shuangshuang",
+        githubLogin: "shuangshuang621",
+    },
+    {
+        username: "simen",
+        githubLogin: "simhex",
+    },
+    {
+        username: "too",
+        githubLogin: "ymtoo",
+    },
+];
